9d371faedaee87c011e8919c5517bf433b3bfd0d
czhang0003 <ciz003@ucsd.edu>
2026-04-28 21:57:46 -0700

    fixed question 16 of part2.md

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,3 +1,12 @@
+let statistic = {
+    redCars: 21,
+    blueCars: 45, 
+    greenCars: 12,
+    raceCars: 5, 
+    blackCars: 40,
+    rareCars: 2
+};
+
 for(const property in statistic){
     if(property.startsWith('r') || statistic[property] % 2 != 0) {
         console.log(statistic[property]);
